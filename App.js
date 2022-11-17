bad680bbc0b4914916d0411d6caf1f977a104bd0
Shivam1919 <shivamthakur.rs143@gmail.com>
2022-11-17 19:03:18 +0530

    Login screen has been updated

--- App.js
+++ App.js
@@ -7,81 +7,111 @@ export default class App extends React.Component {
     password:""
   }
   render(){
-    return (
-      <View style={styles.container}>
-        <Text style={styles.logo}>Zing</Text>
-        <View style={styles.inputView} >
-          <TextInput
-            style={styles.inputText}
-            placeholder="Google"
-            placeholderTextColor="#003f5c"
-            onChangeText={text => this.setState({email:text})}/>
-        </View>
-        <View style={styles.inputView} >
-          <TextInput
-            secureTextEntry
-            style={styles.inputText}
-            placeholder="Password..."
-            placeholderTextColor="#003f5c"
-            onChangeText={text => this.setState({password:text})}/>
-        </View>
-         <TouchableOpacity style={styles.loginBtn}>
-                  <Text style={styles.loginText}>Google</Text>
+     return (
+       <View style={styles.container}>
+         <Text style={styles.logo}>Zing</Text>
+
+
+          <TouchableOpacity style={styles.loginBtn1}>
+                   <Text style={styles.loginText}>Signup with Google</Text>
+                 </TouchableOpacity>
+         <TouchableOpacity style={styles.loginBtn2}>
+           <Text style={styles.loginText}>Signup with facebook</Text>
+         </TouchableOpacity>
+        <TouchableOpacity style={styles.loginBtn3}>
+                  <Text style={styles.loginText}>Sign up with apple</Text>
                 </TouchableOpacity>
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>facebook</Text>
-        </TouchableOpacity>
-       <TouchableOpacity style={styles.loginBtn}>
-                 <Text style={styles.loginText}>Sign up with apple</Text>
-               </TouchableOpacity>
-
-
-      </View>
-    );
+         <TouchableOpacity>
+                   <Text style={styles.or}>------------or------------</Text>
+                 </TouchableOpacity>
+         <TouchableOpacity style={styles.loginBtn4}>
+                          <Text style={styles.loginText}>Continue with Phone</Text>
+                        </TouchableOpacity>
+         <TouchableOpacity>
+                            <Text style={styles.or}>by continuing, you agree to our Terms of service and Privacy Policy</Text>
+                          </TouchableOpacity>
+       </View>
+     );
+   }
   }
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#003f5c',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo:{
-    fontWeight:"bold",
-    fontSize:50,
-    color:"#fb5b5a",
-    marginBottom:40
-  },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#465881",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  inputText:{
-    height:50,
-    color:"white"
-  },
-  forgot:{
-    color:"white",
-    fontSize:11
-  },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#fb5b5a",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
-  loginText:{
-    color:"white"
-  }
-});
+  const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: '#FFFFFF',
+     alignItems: 'center',
+     justifyContent: 'center',
+   },
+   logo:{
+     fontWeight:"bold",
+     fontSize:50,
+     color:"#fb5b5a",
+     marginTop:20,
+     marginBottom:40
+   },
+   inputView:{
+     width:"80%",
+     backgroundColor:"#465881",
+     borderRadius:25,
+     height:50,
+     marginBottom:20,
+     justifyContent:"center",
+     padding:20
+   },
+   inputText:{
+     height:50,
+     color:"white"
+   },
+   or:{
+     color:"black",
+     fontSize:20
+
+   },
+   loginBtn1:{
+     width:"80%",
+     backgroundColor:"#fb5b5a",
+     borderRadius:5,
+     height:50,
+     alignItems:"center",
+     justifyContent:"center",
+     marginTop:20,
+     marginBottom:1
+   },
+   loginBtn2:{
+       width:"80%",
+       backgroundColor:"#61DBFB",
+       borderRadius:5,
+       height:50,
+       alignItems:"center",
+       justifyContent:"center",
+       marginTop:20,
+       marginBottom:1
+     },
+      loginBtn3:{
+           width:"80%",
+           backgroundColor:"#000000",
+           borderRadius:5,
+           height:50,
+           alignItems:"center",
+           justifyContent:"center",
+           marginTop:20,
+           marginBottom:1
+         },
+       loginBtn4:{
+                 width:"80%",
+                 backgroundColor:"#06038D",
+                 borderRadius:5,
+                 height:50,
+                 alignItems:"center",
+                 justifyContent:"center",
+                 marginTop:20,
+                 marginBottom:20
+               },
+
+   loginText:{
+     color:"white"
+   },
+
+  });
+
+
